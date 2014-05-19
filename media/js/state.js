4d884ce3060ed1aa83ea238ea4319110631151cb
Scott Fletcher <sfletche@gmail.com>
2014-05-19 12:31:26 -0700

    adding filter tab detection to loadState

--- media/js/state.js
+++ media/js/state.js
@@ -137,6 +137,8 @@ app.loadCompressedState = function(state) {
         setTimeout( function() { $('#activeTab').tab('show'); }, 200 );
     } else if (state.tab && state.tab === "designs") {
         setTimeout( function() { $('#designsTab').tab('show'); }, 200 );
+    } else if (state.tab && state.tab === "filter") {
+        setTimeout( function() { $('#filterTab').tab('show'); }, 200 );
     } else {
         setTimeout( function() { $('#dataTab').tab('show'); }, 200 );
     }
