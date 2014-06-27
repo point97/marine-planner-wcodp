9577932f896dc127d594f0f6364cdf0462386815
Scott Fletcher <sfletche@gmail.com>
2014-06-27 16:39:56 -0700

    adding ontology app with Term model
    in preparation for the ontology implementation

--- mp/ontology/models.py
+++ mp/ontology/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.gis.db import models
-
-from mptt.models import MPTTModel, TreeForeignKey
+from django.template.defaultfilters import slugify
 
 import caching.base
 
@@ -13,16 +12,3 @@ class Term(caching.base.CachingMixin, models.Model):
     parents = models.ManyToManyField("self", blank=True, null=True, related_name="children")
     # self.children.all()
     objects = caching.base.CachingManager() #caches in redis
-
-class RDFConcept(caching.base.CachingMixin, MPTTModel):
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
-    # URI From RDF for this concept
-    uri = models.CharField(max_length=255)
-    preflabel = models.CharField(max_length=125)
-    definition = models.TextField(null=True, blank=True)
-    slug = models.SlugField(max_length=125, default="", null=False, blank=False)
-
-    objects = caching.base.CachingManager()
-
-    def __unicode__(self):
-        return self.preflabel
