c3de49818c515c1d70cbcca872039fdac553192c
Seth Hill <seth@pointnineseven.com>
2014-10-10 10:55:23 -0700

    MP-277 #comment Add knockout code to handle date boxes.

--- media/js/filters.js
+++ media/js/filters.js
@@ -1,3 +1,19 @@
+ko.bindingHandlers.datePicker = {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+        // Register change callbacks to update the model if the control changes
+        ko.utils.registerEventHandler(element, "change", function() {
+            var value = valueAccessor();
+            value(new Date(element.value));
+        });
+    },
+    // update the control whenever the view model changes
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = valueAccessor(); 
+        value = value(); // get the date
+        element.value = app.dateToString_us(value);
+    }
+}
+
 function filteringModel() {
 	var self = this;
 	
