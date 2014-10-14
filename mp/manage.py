522300d6b874759230dc00c478195297d325dc1b
Seth Hill <seth@pointnineseven.com>
2014-10-13 17:00:06 -0700

    MP-277 #comment fix chrome devserver loading

--- mp/manage.py
+++ mp/manage.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    from django.core.servers.basehttp import WSGIServer
+    WSGIServer.request_queue_size = 10
+
     execute_from_command_line(sys.argv)
