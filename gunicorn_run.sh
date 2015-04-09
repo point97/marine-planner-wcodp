54c170183504a7930b07e720c39f0307f1fdbd0a
Seth Hill <seth@pointnineseven.com>
2015-04-09 15:38:10 -0700

    Use PIDs in the service, otherwise init.d can't tell the difference between multiple services running python (i.e., marine debris and marine planner)

--- gunicorn_run.sh
+++ gunicorn_run.sh
@@ -8,6 +8,7 @@ APPDIR=/usr/local/apps/marine-planner-wcodp
 VENVDIR=/usr/local/venv/marine-planner-wcodp
 GUNICORN=/usr/local/venv/marine-planner-wcodp/bin/gunicorn
 LOGFILE=/var/log/marine-planner-wcodp/gunicorn.log
+PIDFILE=/var/run/marine-planner-wcodp.pid
 TIMEOUT_S=600
 NUM_WORKERS=4
 USER=marine-planner
@@ -19,4 +20,5 @@ exec $GUNICORN $PROJECT.wsgi_wcgardf:application \
      --bind $BIND \
      --workers $NUM_WORKERS \
      --timeout $TIMEOUT_S \
+     --pid $PIDFILE \
      --user=$USER --group=$GROUP --log-level=INFO --log-file=$LOGFILE 2>>$LOGFILE &
