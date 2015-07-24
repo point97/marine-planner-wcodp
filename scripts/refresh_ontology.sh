1edfb9c001a375cd7ba3f4046d9c004efebaa6b4
Seth Hill <seth@pointnineseven.com>
2015-07-24 10:47:16 -0700

    Update virtualenv path for new servers

--- scripts/refresh_ontology.sh
+++ scripts/refresh_ontology.sh
@@ -3,7 +3,7 @@
 # Install in cron with (run once a day at 1am)
 # 0 1 * * * path/to/marine_planner_wcodp_refresh_ontology.sh
 
-source /usr/local/venv/marine-planner-wcodp/bin/activate
+source /usr/local/env/marine-planner-wcodp/bin/activate
 cd /usr/local/apps/marine-planner-wcodp/mp
 python manage.py syncrdf
 
